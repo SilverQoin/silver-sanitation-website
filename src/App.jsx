cf03882299a8d662c6b6c29c81c0e450ee1d40c3
SilverQoin <bw@silversanitation.com>
2025-08-08 10:09:47 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello from Silver Sanitation!</h1>
+      <p>Your React app is now running on Netlify 🚀</p>
+    </div>
+  );
+}
